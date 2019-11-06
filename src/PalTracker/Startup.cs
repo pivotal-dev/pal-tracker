74c0cb93b7e6bf77020585766e93ce63c5c79fec
pivotal-dev <afshaananwar97@gmail.com>
2019-11-06 17:08:34 +0530

    added orm,updated config

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -36,7 +32,8 @@ namespace PalTracker
                                                         Configuration.GetValue<string>("CF_INSTANCE_ADDR","No Addr")
                                                 ));
 
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();                                                
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();  
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));                                             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
