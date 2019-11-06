1f053976633e74e7628604ea5159a44e7cc9e2d7
pivotal-dev <afshaananwar97@gmail.com>
2019-11-06 17:17:48 +0530

    updated the create_db script

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -4,7 +4,7 @@ DROP DATABASE IF EXISTS tracker_dotnet_test;
 CREATE DATABASE tracker_dotnet_dev;
 CREATE DATABASE tracker_dotnet_test;
 
-CREATE USER IF NOT EXISTS 'tracker_dotnet'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVELIGES ON tracker_dotnet_dev.* TO 'tracker_dotnet'@'localhost';
-GRANT ALL PRIVELIGES ON tracker_dotnet_test.* TO 'tracker_dotnet'@'localhost';
-
+CREATE USER IF NOT EXISTS 'tracker_dotnet'@'localhost'
+  IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON tracker_dotnet_dev.* TO 'tracker_dotnet'@'localhost';
+GRANT ALL PRIVILEGES ON tracker_dotnet_test.* TO 'tracker_dotnet'@'localhost';
